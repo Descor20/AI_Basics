6d8cdbaee06e7dec7aa763b77c8a57149a1fac2d
Descor20 <kylian.drif@gmail.com>
2025-02-28 17:24:50 +0100

    [AI]{Memory}(feat :adding constructor and getter and setter)

--- Creeps_AI/src/main/java/com/epita/creeps/AI_manager/template/GraphTemplate.java
+++ Creeps_AI/src/main/java/com/epita/creeps/AI_manager/template/GraphTemplate.java
@@ -10,5 +10,27 @@ public class GraphTemplate {
     public List<List<Double>> bias;
     public List<List<List<Double>>> weigth;
 
+    public GraphTemplate() {
+    }
 
+    public GraphTemplate(List<List<Double>> bias, List<List<List<Double>>> weigth) {
+        this.bias = bias;
+        this.weigth = weigth;
+    }
+
+    public List<List<Double>> getBias() {
+        return bias;
+    }
+
+    public List<List<List<Double>>> getWeigth() {
+        return weigth;
+    }
+
+    public void setBias(List<List<Double>> bias) {
+        this.bias = bias;
+    }
+
+    public void setWeigth(List<List<List<Double>>> weigth) {
+        this.weigth = weigth;
+    }
 }
