805105ccb0bbdfd8dc4f3d7e5c03744c10bc7f66
kylian.drif <kylian.drif@epita.fr>
2025-02-28 17:47:30 +0100

    [AI]{Memory}(feat :adding safety feature)

--- Creeps_AI/src/main/java/com/epita/creeps/tool/GraphKeeper.java
+++ Creeps_AI/src/main/java/com/epita/creeps/tool/GraphKeeper.java
@@ -33,9 +33,10 @@ public class GraphKeeper {
         return exit;
     }
 
-    public void evaluate_all(List<Double> entry) {
+    public boolean evaluate_all(List<Double> entry) {
         if (entry.size() != entries.size()) {
-            System.err.println("[AI manager]{Evaluate} :The entries doesn't match the requiered number of entries.");
+            System.err.println("[AI manager]{Evaluate All} :The entries doesn't match the requiered number of entries.");
+            return false;
         }
         int i = 0;
         for (Graph g : entries) {
@@ -43,6 +44,7 @@ public class GraphKeeper {
             g.evaluate();
             i++;
         }
+        return true;
     }
 
 
@@ -51,44 +53,7 @@ public class GraphKeeper {
         JSON_Memory.write(this, path);
     }
 
-    private void set_previous_layer() {
-        List<Graph> g = new ArrayList<>();
-        for (int i = 0; i < entries.size(); i++) {
-            g.add(entries.get(0));
-        }
-
-        while (g.size() > 0){
-            Graph entry = g.get(0);
-            List<Graph> previous = new ArrayList<>();
-
-            List<Graph> it = new ArrayList<>();
-            for (int j = 0; j < entries.size(); j++) {
-                it.add(entries.get(j));
-            }
-            while (it.size() > 0) {
-                Graph prev = it.get(0);
-                if (Basics.contains(prev.next, entry.id)) {
-                    previous.add(prev);
-                }
-                for (int i = 0; i < prev.getNext().size(); i++) {
-                    it.add(prev.getNext().get(i));
-                }
-                it.remove(prev);
-            }
-
-            entry.setPrevious(previous); // Don't work, is it a copy !?
-
-
-            for (int i = 0; i < entry.getNext().size(); i++) {
-                g.add(entry.getNext().get(i));
-            }
-            g.remove(entry);
-        }
-
-    }
     public static GraphKeeper retrieve(String path) {
-        GraphKeeper keeper = JSON_Memory.read_graph(path);
-        keeper.set_previous_layer();
-        return keeper;
+        return JSON_Memory.read_graph(path);
     }
 }
